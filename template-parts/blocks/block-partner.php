c4253b5908710577ffe82818c7941a5294c90f43
eLDoherty <leonardodoherty@gmail.com>
2021-07-01 10:28:23 +0700

    Block Partner

---
+++ template-parts/blocks/block-partner.php
@@ -0,0 +1,23 @@
+<?php
+
+$images = get_sub_field( 'items' );
+
+?>
+  
+    <section class="bg--secondary partners-1 space--sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="slider" data-items="6" data-timing="3000">
+                        <ul class="slides">
+                        <?php foreach( $images as $image ) : ?>
+                            <li>
+                                <img alt="img" src="<?php echo $image; ?>" />
+                            </li> 
+                        <?php endforeach; ?>                         
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
